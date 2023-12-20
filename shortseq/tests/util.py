684f91eb80f7b0b0d531a409af4e182b7c28768e
Alex Tate <0xalextate@gmail.com>
2023-12-20 12:14:54 -0700

    Adding a basic benchmarking suite and plots to visualize performance.

--- shortseq/tests/util.py
+++ shortseq/tests/util.py
@@ -1,5 +1,6 @@
 import random
 import math
+import re
 
 
 def print_var_seq_pext_chunks(seq):
@@ -37,3 +38,17 @@ def rand_sequence(min_length=None, max_length=None, as_bytes=False):
         seq = ''.join(random.choice(bases) for _ in range(min_length))
 
     return seq.encode() if as_bytes else seq
+
+
+def sorted_natural(lines, key=None, reverse=False):
+    """Sorts alphanumeric strings with entire numbers considered in the sorting order,
+    rather than the default behavior which is to sort by the individual ASCII values
+    of the given number. Returns a sorted copy of the list, just like sorted().
+
+    Not sure who to credit... it seems this snippet has been floating around for quite
+    some time. Strange that there isn't something in the standard library for this."""
+
+    convert = lambda text: int(text) if text.isdigit() else text.lower()
+    extract = (lambda data: key(data)) if key is not None else lambda x: x
+    alphanum_key = lambda elem: [convert(c) for c in re.split(r'(\d+)', extract(elem))]
+    return sorted(lines, key=alphanum_key, reverse=reverse)
